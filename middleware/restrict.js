ab8c1ae35ee91eb0efabd9a278f02bf46a775660
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-12 14:57:13 -0400

    Create restrict.js

---
+++ middleware/restrict.js
@@ -0,0 +1,22 @@
+
+function restrict() {
+	const error = {
+		message: 'Invalid credentials',
+	}
+
+	return async (req, res, next) => {
+		try {
+
+			if (!req.session || !req.session.user) {
+				return res.status(401).json(error);
+			}
+
+			// if we reach this point, the user is considered authorized!
+			next();
+		} catch (err) {
+			next(err);
+		}
+	}
+}
+
+module.exports = restrict;
