b6ca807086a74537b672ba37d3c0ad840a2af5c9
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-12 15:55:55 -0400

    Cleaned up files

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,6 @@
 // Update with your config settings.
 
 module.exports = {
-
-  development: {
     client: 'sqlite3',
     connection: {
       filename: './data/user-tracker.db3'
@@ -21,37 +19,4 @@ module.exports = {
         conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
       },
     },
-  },
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+}
