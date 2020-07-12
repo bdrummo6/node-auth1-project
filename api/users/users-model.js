6d466abcbf5738a4f7296a4b004138d48e912f8c
bdrummo6 <bradley.drummonds@gmail.com>
2020-07-12 13:44:04 -0400

    Setup adding user endpoint without encryption

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,12 +1,32 @@
 const db = require('../../data/dbConfig');
 
+
 // Finds every users id and username from users table in database
 function find() {
 	return db('users').select('id', 'username');
 }
 
+// Finds user with the specified id
+function findById(id) {
+	return db('users').select('id', 'username').where({ id }).first();
+}
+
+// Finds user by specified criteria
+function findBy(filter) {
+	return db('users').select('id', 'username', 'password').where(filter);
+}
+
+// Adds user to the database
+async function add(user) {
+	const [id] = await db('users').insert(user);
+	return findById(id);
+}
+
 module.exports = {
-	find
+	find,
+	findBy,
+	findById,
+	add
 }
 
 
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -12,4 +12,27 @@ router.get('/users', async (req, res, next) => {
 	}
 })
 
+// Adds a new user to the database
+router.post('/register', async (req, res, next) => {
+	try {
+		const { username, password } = req.body;
+		const user = await Users.findBy({ username }).first();
+
+		if (user) {
+			return res.status(409).json({
+				message: 'Username already exists!',
+			})
+		}
+
+		const newUser = await Users.add({
+			username,
+			password
+		})
+
+		res.status(201).json(newUser);
+	} catch(err) {
+		next(err);
+	}
+})
+
 module.exports = router;
